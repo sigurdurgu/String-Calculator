d80f34e46276a5508a4795ef13d0f6c8cd679dc5
Sigurdur <s14@ru.is>
2016-10-15 22:28:16 +0000

    testOneNumber

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -5,13 +5,16 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	
 
 	@Test
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
 
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+	}
 		
 	
 }
